c8b12d25ad13688d35438372c2fc954ff8b2ec86
Limdeokhan <limdh001@gmail.com>
2023-12-13 23:44:34 +0900

    Deposit Withdrawl
    입금 출금 잔액부족 구현

--- Assets/Scripts/BankManager.cs
+++ Assets/Scripts/BankManager.cs
@@ -1,14 +1,104 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BankManager : MonoBehaviour
 {
     public GameObject DepositPanel;
     public GameObject WithdrawlPanel;
     public GameObject DepoWithPanel;
-    
+    public GameObject StopPanel;
 
+    public Text Cash;
+    public Text Balance;
+
+    public int cash = 100000;
+    public int balance = 50000;
+    public void DepositTen()
+    {      
+        if (cash < 10000)
+        {
+            //StopPanel.SetActive(true);
+        }
+        else
+        {
+            cash -= 10000;
+            balance += 10000;
+        }
+    }
+
+    public void DepositThirty()
+    {
+        if (cash < 30000)
+        {
+            StopPanel.SetActive(true);
+        }
+        else
+        {
+            cash -= 30000;
+            balance += 30000;
+        }
+    }
+    public void DepositFifty()
+    {
+        if (cash < 50000)
+        {
+            StopPanel.SetActive(true);
+        }
+        else
+        {
+            cash -= 50000;
+            balance += 50000;
+        }
+    }
+
+    public void WithdrawlTen()
+    {
+        if (balance < 10000)
+        {
+            StopPanel.SetActive(true);
+        }
+        else
+        {
+            balance -= 10000;
+            cash += 10000;
+        }
+    }
+    public void WithdrawlThirty()
+    {
+        if (balance < 30000)
+        {
+            StopPanel.SetActive(true);
+        }
+        else
+        {
+            balance -= 30000;
+            cash += 30000;
+        }
+    }
+    public void WithdrawlFifty()
+    {
+        if (balance < 50000)
+        {
+            StopPanel.SetActive(true);
+        }
+        else
+        {
+            balance -= 50000;
+            cash += 50000;
+        }
+    }
+    public void SettingCashBalance()
+    {
+        Cash.text = "Çö±Ý: " + cash.ToString();
+        Balance.text = "Balance: " + balance.ToString();
+    }
+
+    public void OnClickStop()
+    {
+        StopPanel.SetActive(false);
+    }
     public void OnClickDeposit()
     {
         DepositPanel.SetActive(true);
@@ -38,7 +128,10 @@ public class BankManager : MonoBehaviour
         WithdrawlPanel.SetActive(false);
     }
 
-
+    void Awake()
+    {
+        SettingCashBalance();
+    }
     void Start()
     {
         
@@ -47,6 +140,6 @@ public class BankManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        SettingCashBalance();
     }
 }
